8cdaa84ef65b6c7cac30b8c19da1e3585f314224
Marco Moura <email@marcomoura.com>
2012-09-17 15:15:34 -0300

    install node.js

--- manifests/package.pp
+++ manifests/package.pp
@@ -22,8 +22,17 @@ package{'libcurl4-openssl-dev': }
 package{'curl': } 
 package{'python-software-properties': }
 
-#exec{ 'node': command => 'apt-add-repository ppa:chris-lea/node.js', }
-#exec{ 'update': command => 'apt-get -y update', }
-#package{'nodejs': }
+exec { "add node ppa":
+    command => "/usr/bin/apt-add-repository ppa:chris-lea/node.js",
+    creates => "/etc/apt/sources.list.d/kilian-node-maverick.list",
+    notify => Exec["apt-get update"],
+  }
+
+exec { "apt-get update":
+        command => "/usr/bin/apt-get update",
+        refreshonly => true;
+    }
+
+package{'nodejs': }
 
 
